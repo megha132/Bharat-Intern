1f52841517868548a9fb6e67da8f8c2cab97b067
megha132 <103588101+megha132@users.noreply.github.com>
2023-12-10 11:16:44 +0530

    Task 1 sms spam classifier

---
+++ sms-spam-classifier/app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer 
+
+model = pickle.load(open('model.pkl', 'rb'))
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PortStemmer
+import string
+
+nltk.download('stopwords')
+
+ps = PorterStemmer()
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+    text = [word for word in text if word.isalnum()]
+    text = [word for word in text if word not in stopwords.words('english') and word not in string.punctuation]
+    text = [ps.stem(word) for word in text]
+    return " ".join(y)
+
+st.title("SMS Spam Classifier")
+input_sms = st.text_area("Enter the message")
+
+if st.button('Predict'):
+    transformed_sms = transform_text(input_sms)
+    vector_input = tfidf.transform([transformed_sms])
+    result = model.predict(vector_input)[0]
+
+    if result ==1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam")
